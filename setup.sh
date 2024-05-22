c3b27018be7bef29f46c8c2a26c1b4d6ffc2e271
Stefan Pejcic <stefan@pejcic.rs>
2024-05-22 12:36:22 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,12 @@
 # this script should be run only on servers that have OpenPanel installed.
 # to use OpenPanel-FTP as a standalone ftp server, check the readme
 
+config_file="/usr/local/panel/conf/panel.config"
+
+
 # OpenPanel?
 check_openpanel_installed() {
-  if [ -d "/usr/local/panel/" ]; then
+  if [ -f $config_file ]; then
     return 0
   else
     return 1
@@ -43,11 +46,13 @@ if check_openpanel_installed; then
   mkdir -p /etc/openpanel/ftp/users/
 
   # run the container
+  echo "Starting docker container.."
   run_docker_container
 
   # if container started, we continue..
   if docker ps --filter "name=openadmin_ftp" --filter "status=running" | grep -q openadmin_ftp; then
-  
+
+    echo "Copying OpenPanel module and OpenAdmin extension files.."
     # OpenPanel module
     cp module/ftp.py /usr/local/panel/modules/ftp.py
     cp module/ftp.html /usr/local/panel/templates/ftp.html
@@ -56,13 +61,27 @@ if check_openpanel_installed; then
     cp module/admin/ftp.py /usr/local/admin/modules/ftp.py
     cp module/admin/ftp.html /usr/local/admin/templates/ftp.html
   
+  # Check if 'ftp' is in the enabled_modules line
+  if grep -q "^enabled_modules=.*ftp" "$config_file"; then
+    # FTP already exists in enabled modules!
+    echo "FTP Module is already enabled in OpenPanel configuration."
+  else
+    # nope
+    echo "Adding FTP to enabled_modules for OpenPanel."
+    sed -i '/^enabled_modules=/ s/$/,ftp/' "$config_file"
+  fi
+    
+  
     # reload services
+    echo "Reloading OpenPanel and OpenAdmin services to apply changes."
     service admin reload
     service panel reload
     
   else
     # Oops!
+    echo ""
     echo "ERROR: openadmin_ftp container is not running. Exiting..."
+    echo ""
     docker stop openadmin_ftp
     docker rm openadmin_ftp
     exit 1
@@ -70,8 +89,11 @@ if check_openpanel_installed; then
 
   # Open the required ports
   open_ports
-
-  echo "SUCCESS: FTP is running and enabled for all OpenPanel users"
+  echo ""
+  echo "SUCCESS: FTP is now running and enabled for all OpenPanel users."
+  echo ""
 else
+  echo ""
   echo "ERROR: OpenPanel is not installed. To run a standalone FTP without OpenPanel, please read the README.md file."
+  echo ""
 fi
