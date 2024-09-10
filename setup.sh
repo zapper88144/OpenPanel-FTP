5cdbd3a3a49291c9dd17b8346174e5cf5a46a301
Stefan Pejcic <stefan@pejcic.rs>
2024-09-10 15:48:29 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ PANEL_CONFIG="/etc/openpanel/openpanel/conf/openpanel.config"
 GIT_URL="https://github.com/stefanpejcic/OpenPanel-FTP/archive/refs/heads/master.zip"
 ETC_DIR="/etc/openpanel/ftp/users/"
 
-# apt-get install zip -y
+apt-get install zip -y
 
 # OpenPanel?
 check_openpanel_installed() {
@@ -20,17 +20,7 @@ check_openpanel_installed() {
 
 # run container
 run_docker_container() {
-
-
-
-  docker run -d \
-    -p "21:21" \
-    -p 21000-21010:21000-21010 \
-    --restart=always \
-    --name=openadmin_ftp \
-    --mount type=bind,source=/home,target=/home \
-    --memory="1g" --cpus="1" \
-    openpanel/ftp
+  cd /root && docker compose up openadmin_ftp -d
 }
 
 
@@ -90,47 +80,12 @@ cleanup() {
 # Main script
 if check_openpanel_installed; then
   echo "OpenPanel is installed. Proceeding with Docker container setup and port opening."
-
-  echo "Downloading the latest openpanel/ftp image and module."  
-
-  # create dir
-  mkdir -p $ETC_DIR
-
-  # download from github
-  wget $GIT_URL -O file.zip
-  unzip file.zip  
-
-  echo "Building docker image.."
-  # build image for now, later download from dockerhub
-  cd OpenPanel-FTP-master && docker build . -t "openpanel/ftp"
-  cd ..
-  
-  # chech if image exists
-  if docker images | grep -q "openpanel/ftp"; then
-      # run the container
-      echo "Starting docker container.."
       run_docker_container
-  else
-    # Oops!
-    echo ""
-    echo "ERROR: The Docker image 'openpanel/ftp' does not exist. Exiting..."
-    echo ""
-    
-    cleanup
   fi
 
 
   # if container started, we continue..
   if docker ps --filter "name=openadmin_ftp" --filter "status=running" | grep -q openadmin_ftp; then
-
-    echo "Copying OpenPanel module and OpenAdmin extension files.."
-    # OpenPanel module
-    cp module/ftp.py /usr/local/panel/modules/ftp.py
-    cp module/ftp.html /usr/local/panel/templates/ftp.html
-  
-    # OpenAdmin extension
-    cp module/admin/ftp.py /usr/local/admin/modules/ftp.py
-    cp module/admin/ftp.html /usr/local/admin/templates/ftp.html
   
   # Check if 'ftp' is in the enabled_modules line
   if grep -q "^enabled_modules=.*ftp" "$PANEL_CONFIG"; then
@@ -145,7 +100,7 @@ if check_openpanel_installed; then
   
     # reload services
     echo "Reloading OpenPanel and OpenAdmin services to apply changes."
-    service admin reload
+    #service admin reload
     docker restart openpanel
     
   else
